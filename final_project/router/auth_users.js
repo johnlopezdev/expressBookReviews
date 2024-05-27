60bbd9f11b1b9e9bcfe6fe1859ba42e10afb0a5d
John Lopez <john.lopez.developer@gmail.com>
2024-05-27 09:47:53 +0800

    Project init, port change

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -5,16 +5,16 @@ const regd_users = express.Router();
 
 let users = [];
 
-const isValid = (username)=>{ //returns boolean
+const isValid = (username) => { //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
+const authenticatedUser = (username, password) => { //returns boolean
 //write code to check if username and password match the one we have in records.
 }
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
+regd_users.post("/login", (req, res) => {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
 });
